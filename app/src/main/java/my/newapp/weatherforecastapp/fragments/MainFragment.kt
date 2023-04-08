17878ef3cd9eac9d4736163c001a093eaf236b44
fsm13 <fomichevsm@gmail.com>
2023-04-08 17:46:37 +0700

    creating DataClass and permission functions

--- app/src/main/java/my/newapp/weatherforecastapp/fragments/MainFragment.kt
+++ app/src/main/java/my/newapp/weatherforecastapp/fragments/MainFragment.kt
@@ -1,22 +1,43 @@
 package my.newapp.weatherforecastapp.fragments
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import my.newapp.weatherforecastapp.R
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import my.newapp.weatherforecastapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
-
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener() {
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()) {
+
+        }
+    }
+
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
