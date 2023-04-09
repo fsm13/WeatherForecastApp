c2e21caad865fbb7554d04c57f5eb23ccc1b4d4f
fsm13 <fomichevsm@gmail.com>
2023-04-09 14:59:17 +0700

    creating viewPager2 and tablayout

--- app/src/main/java/my/newapp/weatherforecastapp/fragments/MainFragment.kt
+++ app/src/main/java/my/newapp/weatherforecastapp/fragments/MainFragment.kt
@@ -8,11 +8,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import my.newapp.weatherforecastapp.VPAdapter
 import my.newapp.weatherforecastapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private val fraglist = listOf(HoursFragment.newInstance(), DaysFragment.newInstance())
+    private val tabTitleList = listOf("HOURS", "DAYS")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,6 +29,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    fun init() {
+        val adapter = VPAdapter(activity as FragmentActivity, fraglist)
+        binding.vpMain.adapter = adapter
+        TabLayoutMediator(binding.tlMain, binding.vpMain) {
+                tab, pos -> tab.text = tabTitleList[pos]
+        }.attach()
     }
 
     private fun permissionListener() {
